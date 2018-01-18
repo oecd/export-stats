2664f059ec253ed204b7fd28d7b2aca241837b26
Jakob Fix <jakob@jfix.com>
2018-01-18 23:50:05 +0100

    very early version but works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/* global $ */
+const threshold = 95
+const elt = $('#export-percentage')
 
+function displayStats () {
+  $.getJSON('https://untitled-y13mrm2i2lqe.runkit.sh/', function (data) {
+    const percentage = Math.round(data.success * 100 / data.total)
+    elt.text(`${percentage}%`)
+    if (percentage >= threshold) {
+      elt.css('color', 'green')
+    } else {
+      elt.css('color', 'red')
+    }
+  })
+}
+
+displayStats()
