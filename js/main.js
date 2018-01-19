a9f0b8467ab6cf2ec02e8ba1d757726ce4479395
Jakob Fix <jakob@jfix.com>
2018-01-19 12:32:10 +0100

    Some more information displayed.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 /* global $ */
 const threshold = 95
-const elt = $('#export-percentage')
+const percentageElt = $('#export-percentage')
+const explanationElt = $('#export-explanation')
 
 function displayStats () {
   $.getJSON('https://untitled-y13mrm2i2lqe.runkit.sh/', function (data) {
-    const percentage = Math.round(data.success * 100 / data.total)
-    elt.text(`${percentage}%`)
+    const total = data.total
+    const success = data.success
+    const failures = total - success
+
+    const percentage = Math.round(success * 100 / total)
+    percentageElt.text(`${percentage}%`)
+    explanationElt.text(`95% is the percentage to strive for.  Out of ${total} exports, ${failures} failed.`)
     if (percentage >= threshold) {
-      elt.css('color', 'green')
+      percentageElt.css('color', 'green')
     } else {
-      elt.css('color', 'red')
+      percentageElt.css('color', 'red')
     }
   })
 }
