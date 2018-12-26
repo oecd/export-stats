270394f4cd5bf496decc75cd1221a3bbe5cc8582
Jakob Fix <jakob@jfix.com>
2018-12-26 16:32:59 +0100

    Use export date, not current date (fixes #4)

--- lambda/handler.js
+++ lambda/handler.js
@@ -4,14 +4,22 @@ module.exports.nudger = (event, context, callback) => {
   require('dotenv').config()
   const request = require('request')
   const moment = require('moment')()
+  let dateString
+  let formattedDate
 
-  const date = (moment.day() <= 1 || moment.day() > 5)
-    ? moment.day(-2).format('dddd')
-    : 'yesterday'
-  console.log(`Moment is: ${moment}`)
+  if (moment.day() <= 1 || moment.day() > 5) {
+    const friday = moment.day(-2) // -2 === Friday
+    formattedDate = friday.format('YYYYMMDD')
+    dateString = friday.format('dddd')
+  } else {
+    const yesterday = moment.subtract(1, 'days') // yesterday
+    dateString = 'yesterday'
+    formattedDate = yesterday.format('YYYYMMDD') // yesterday's date
+  }
+  
   const slackWebhook = process.env.SLACK_WEBHOOK
   const slackOptions = {
-    'text': `Hello there :wave:! Did ${date}'s export go OK? If you're unsure, <https://pacps01.oecd.org/Dissemination|check here>.`,
+    'text': `Hello there :wave:! Did ${dateString}'s export go OK? If you're unsure, <https://pacps01.oecd.org/Dissemination|check here>.`,
     'attachments': [
       {
         'text': 'Please click the corresponding button:',
@@ -22,14 +30,14 @@ module.exports.nudger = (event, context, callback) => {
         'actions': [
           {
             'name': 'export',
-            'value': 'ok',
+            'value': `${formattedDate}-ok`,
             'text': 'Yes! :+1:',
             'type': 'button',
             'style': 'primary'
           },
           {
             'name': 'export',
-            'value': 'ko',
+            'value': `${formattedDate}-ko`,
             'text': 'No! :disappointed:',
             'type': 'button',
             'style': 'danger'
