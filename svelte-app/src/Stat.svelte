ae0cc7083a9184b0ff2e96643a4aed08676f141e
Jakob Fix <jakob.fix@gmail.com>
2021-09-15 11:14:09 +0200

    Make changes that I'm not sure about
    I was not aware the API format had changed, so I'm surprised
    that the keys don't work as before. :worried:

--- svelte-app/src/Stat.svelte
+++ svelte-app/src/Stat.svelte
@@ -6,9 +6,9 @@
     const statColor = (val) => val >= 95 ? 'success' : val >= 90 ? 'warning' : 'danger'
     const header = (val) => {
         switch (val) {
-            case 'allTime':
-                return 'All time'
-            case 'thirty':
+            case 'alltime':
+                return 'This year'
+            case 'month':
                 return 'Last 30 days'
             case 'hundred':
                 return 'Last 100 days'
@@ -20,18 +20,18 @@
     }
     const calcPercentage = (stats, period) => {
         switch (period) {
-            case 'currentYear':
+            case 'alltime':
                 total = stats.currentYearTotal
-                success = stats.currentYearSuccess
+                success = stats.success
                 break;
             default:
-                total = stats.overallTotal
-                success = stats.overallSuccess
+                total = stats.total
+                success = stats.success
                 break;
         }
         percentage = Math.round(success * 100/total)
     }
-
+    
     $: {
         // period && console.log(`Done!`)
         period && calcPercentage(data, period)
