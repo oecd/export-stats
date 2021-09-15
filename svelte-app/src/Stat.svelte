67edfe788e5fd2c28910b79f85a1be83c16ff8a1
Jakob Fix <jakob.fix@gmail.com>
2021-09-16 00:18:34 +0200

    Revert "Make changes that I'm not sure about"
    This reverts commit ae0cc7083a9184b0ff2e96643a4aed08676f141e.

--- svelte-app/src/Stat.svelte
+++ svelte-app/src/Stat.svelte
@@ -6,9 +6,9 @@
     const statColor = (val) => val >= 95 ? 'success' : val >= 90 ? 'warning' : 'danger'
     const header = (val) => {
         switch (val) {
-            case 'alltime':
-                return 'This year'
-            case 'month':
+            case 'allTime':
+                return 'All time'
+            case 'thirty':
                 return 'Last 30 days'
             case 'hundred':
                 return 'Last 100 days'
@@ -20,18 +20,18 @@
     }
     const calcPercentage = (stats, period) => {
         switch (period) {
-            case 'alltime':
+            case 'currentYear':
                 total = stats.currentYearTotal
-                success = stats.success
+                success = stats.currentYearSuccess
                 break;
             default:
-                total = stats.total
-                success = stats.success
+                total = stats.overallTotal
+                success = stats.overallSuccess
                 break;
         }
         percentage = Math.round(success * 100/total)
     }
-    
+
     $: {
         // period && console.log(`Done!`)
         period && calcPercentage(data, period)
